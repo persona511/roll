bf6541cd90491e58feec0acb02f312020821dd42
persona511 <120021089+persona511@users.noreply.github.com>
2022-12-08 15:57:33 +0200

    Add files via upload

---
+++ roll.css
@@ -0,0 +1,139 @@
+* {
+  margin: 0px;
+  padding: 0px;
+}
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  background-image: url(file:///C:/Users/Данил/Desktop/2582114-vecteur-abstrait-moderne-fond-casino-avec-cartes-a-jouer-bleu-brillant-signes-poker-symboles-sur-fond-noir-casino-symbols-widescreen-wallpaper-vectoriel.jpg);
+  overflow: hidden;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
+
+main{
+  display: flex;
+  gap: 0;
+  justify-content: center;
+  align-items: center;
+}
+
+
+
+.container {
+  width: 600px;
+  height: 600px;
+  background-color: #ccc;
+  border-radius: 50%;
+  border: 15px solid #dde;
+  position: relative;
+  overflow: hidden;
+  transition: 10s;
+  transform: rotate(0deg);
+}
+.container div {
+  height: 50%;
+  width: 342px;
+  transform-origin: bottom;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 50px;
+  font-weight: bold;
+  font-family: sans-serif;
+  color: rgb(247, 10, 10);
+  left: 135px;
+}
+
+.container .one {
+  background-color: rgb(8, 4, 255);
+  width: 100%;
+}
+.container .two {
+  background-color: #fbff00;
+  transform: rotate(360deg);
+  width: 100%;
+
+}
+
+.ticker {
+  bottom: 43%;
+  left: 31%;
+  transform: translateX(-50%);
+  transform: rotate(270deg);
+  color: rgb(255, 0, 0);
+  z-index: 5;
+}
+.ticker::before {
+  content: "\1f817";
+  font-size: 100px;
+  bottom: 43%;
+  left: 31%;
+
+}
+#spin {
+  position: absolute;
+  top: 50%;
+  left: 51.4%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  background-color: #e2e2e2;
+  text-transform: uppercase;
+  border: 8px solid #e2e2e2;
+  font-weight: bold;
+  font-size: 20px;
+  color: #000000;
+  width: 80px;
+  height: 80px;
+  font-family: sans-serif;
+  border-radius: 50%;
+  cursor: pointer;
+  outline: none;
+  letter-spacing: 1px;
+}
+#freecash {
+  position: absolute;
+  top: 90%;
+  left: 51.4%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  background-color: #ffffff;
+  text-transform: uppercase;
+  border: 8px solid rgb(81, 167, 238);
+  font-weight: bold;
+  font-size: 20px;
+  color: #f81010;
+  width: 300px;
+  height: 80px;
+  font-family: sans-serif;
+  border-radius: 50%;
+  cursor: pointer;
+  outline: none;
+  letter-spacing: 1px;
+} 
+
+/* анимация вращения */
+.is-spinning .spinner {
+  transition: transform 8s cubic-bezier(0.1, -0.01, 0, 1);
+}
+
+/* анимация движения язычка */
+.is-spinning .ticker {
+          animation: tick 700ms cubic-bezier(0.34, 1.56, 0.64, 1);
+}
+
+
+/* эффект, когда колесо задевает язычок при вращении */
+@keyframes tick {
+  40% {
+    /* чуть поворачиваем язычок наверх в середине анимации */
+    transform: rotate(-12deg);
+  }
+}
